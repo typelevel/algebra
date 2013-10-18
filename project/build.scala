4d7216a55bbcc034ea6bc8237b71a9af8463345b
Erik Osheim <d_m@plastic-idolatry.com>
2013-10-18 00:55:21 -0400

    publishing/release set up is implemented but untested.

--- project/build.scala
+++ project/build.scala
@@ -1,22 +1,18 @@
 import sbt._
 import sbt.Keys._
 
-// import sbtunidoc.Plugin._
-// import sbtunidoc.Plugin.UnidocKeys._
-// 
-// import com.typesafe.sbt.pgp.PgpKeys._
-// 
-// import sbtrelease._
+import com.typesafe.sbt.pgp.PgpKeys._
+ 
+import sbtrelease._
 import sbtrelease.ReleasePlugin._
-// import sbtrelease.ReleasePlugin.ReleaseKeys._
-// import sbtrelease.ReleaseStateTransformations._
-// import sbtrelease.Utilities._
-// 
-// import sbtbuildinfo.Plugin._
-
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
+import sbtrelease.Utilities._
+ 
 object AlgebraBuild extends Build {
 
   override lazy val settings = super.settings ++ Seq(
+    name := "algebra",
     organization := "org.spire-math",
     scalaVersion := "2.10.2",
     crossScalaVersions := Seq("2.9.3", "2.10.2"),
@@ -27,9 +23,37 @@ object AlgebraBuild extends Build {
     )
   )
 
-
-  lazy val algebraSettings = Defaults.defaultSettings ++ releaseSettings
-
   lazy val spire = Project("algebra", file(".")).
     settings(algebraSettings: _*)
+
+  lazy val algebraSettings = Defaults.defaultSettings ++ releaseSettings ++ Seq(
+    releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runTest,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      publishSignedArtifacts,
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    )
+  )
+
+  lazy val publishSignedArtifacts = ReleaseStep(
+    action = st => {
+      val extracted = st.extract
+      val ref = extracted.get(thisProjectRef)
+      extracted.runAggregated(publishSigned in Global in ref, st)
+    },
+    check = st => {
+      // getPublishTo fails if no publish repository is set up.
+      val ex = st.extract
+      val ref = ex.get(thisProjectRef)
+      Classpaths.getPublishTo(ex.get(publishTo in Global in ref))
+      st
+    },
+    enableCrossBuild = true
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-//addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
 
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.7.1")
 
