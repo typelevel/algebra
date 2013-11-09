612eabfcc94147445eb6d1d39d7708240e174ecd
Erik Osheim <d_m@plastic-idolatry.com>
2013-11-08 23:11:56 -0500

    reverse the order of type params

--- src/main/scala/algebra/Priority.scala
+++ src/main/scala/algebra/Priority.scala
@@ -1,40 +1,40 @@
 package algebra
 
-sealed trait Priority[+F, +P] {
-  def fold[B](f1: F => B)(f2: P => B): B =
+sealed trait Priority[+P, +F] {
+  def fold[B](f1: P => B)(f2: F => B): B =
     this match {
-      case Fallback(x) => f1(x)
-      case Preferred(y) => f2(y)
+      case Preferred(x) => f1(x)
+      case Fallback(y) => f2(y)
     }
 
-  def join[U >: F with P]: U = fold(_.asInstanceOf[U])(_.asInstanceOf[U])
+  def join[U >: P with F]: U = fold(_.asInstanceOf[U])(_.asInstanceOf[U])
 
-  def bimap[F2, P2](f1: F => F2)(f2: P => P2): Priority[F2, P2] =
+  def bimap[P2, F2](f1: P => P2)(f2: F => F2): Priority[P2, F2] =
     this match {
-      case Fallback(x) => Fallback(f1(x))
-      case Preferred(y) => Preferred(f2(y))
+      case Preferred(x) => Preferred(f1(x))
+      case Fallback(y) => Fallback(f2(y))
     }
 
-  def toEither: Either[F, P] = fold[Either[F, P]](f => Left(f))(p => Right(p))
+  def toEither: Either[P, F] = fold[Either[P, F]](p => Left(p))(f => Right(f))
 
-  def isPreferred: Boolean = fold(_ => false)(_ => true)
-  def isFallback: Boolean = fold(_ => true)(_ => false)
+  def isPreferred: Boolean = fold(_ => true)(_ => false)
+  def isFallback: Boolean = fold(_ => false)(_ => true)
 
-  def getPreferred: Option[P] = fold[Option[P]](_ => None)(p => Some(p))
-  def getFallback: Option[F] = fold[Option[F]](f => Some(f))(_ => None)
+  def getPreferred: Option[P] = fold[Option[P]](p => Some(p))(_ => None)
+  def getFallback: Option[F] = fold[Option[F]](_ => None)(f => Some(f))
 }
 
-case class Preferred[P](get: P) extends Priority[Nothing, P]
-case class Fallback[F](get: F) extends Priority[F, Nothing]
+case class Preferred[P](get: P) extends Priority[P, Nothing]
+case class Fallback[F](get: F) extends Priority[Nothing, F]
 
 object Priority extends FindPreferred {
-  def apply[F, P](implicit ev: Priority[F, P]): Priority[F, P] = ev
+  def apply[P, F](implicit ev: Priority[P, F]): Priority[P, F] = ev
 }
 
 trait FindPreferred extends FindFallback {
-  implicit def preferred[P](implicit ev: P): Priority[Nothing, P] = Preferred(ev)
+  implicit def preferred[P](implicit ev: P): Priority[P, Nothing] = Preferred(ev)
 }
 
 trait FindFallback {
-  implicit def fallback[F](implicit ev: F): Priority[F, Nothing] = Fallback(ev)
+  implicit def fallback[F](implicit ev: F): Priority[Nothing, F] = Fallback(ev)
 }
