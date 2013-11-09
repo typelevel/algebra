7e7709a5e192ff7c1ec173af99eb1b9b5d55a4f7
Erik Osheim <d_m@plastic-idolatry.com>
2013-11-09 11:47:08 -0500

    update release settings

--- project/build.scala
+++ project/build.scala
@@ -14,12 +14,44 @@ object AlgebraBuild extends Build {
   override lazy val settings = super.settings ++ Seq(
     name := "algebra",
     organization := "org.spire-math",
+
     scalaVersion := "2.10.2",
     crossScalaVersions := Seq("2.9.3", "2.10.2"),
+
     scalacOptions ++= Seq(
       "-deprecation",
       "-unchecked",
       "-optimize"
+    ),
+
+    licenses := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
+    homepage := Some(url("http://spire-math.org")),
+
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+
+    publishTo <<= version {
+      (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) 
+        Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+
+    pomExtra := (
+<scm>
+  <url>git@github.com:non/algebra.git</url>
+  <connection>scm:git:git@github.com:non/algebra.git</connection>
+</scm>
+<developers>
+  <developer>
+    <id>d_m</id>
+    <name>Erik Osheim</name>
+    <url>http://github.com/non/</url>
+  </developer>
+</developers>
     )
   )
 
