a5df4374f7b19d732953d30fcdbc98df98ab59fd
Adelbert Chang <adelbertc@gmail.com>
2015-06-09 21:59:44 -0700

    Fewer passes for PartialOrder[Set]

--- std/src/main/scala/algebra/std/set.scala
+++ std/src/main/scala/algebra/std/set.scala
@@ -20,9 +20,9 @@ class SetLattice[A] extends Lattice[Set[A]] {
 
 class SetPartialOrder[A] extends PartialOrder[Set[A]] {
   def partialCompare(x: Set[A], y: Set[A]): Double =
-    if (x == y) 0.0
-    else if (y.subsetOf(x)) 1.0
-    else if (x.subsetOf(y)) -1.0
+    if (x.size < y.size) if (x.subsetOf(y)) -1.0 else Double.NaN
+    else if (y.size < x.size) -partialCompare(y, x)
+    else if (x == y) 0.0
     else Double.NaN
 
   override def eqv(x: Set[A], y: Set[A]): Boolean = x == y
