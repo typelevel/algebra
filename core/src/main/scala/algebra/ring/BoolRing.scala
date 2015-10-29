e246512eae98781f5e2dcce8c3160f91f8e14979
Tomas Mikula <tomas.mikula@gmail.com>
2015-10-28 22:45:24 -0400

    Document BoolRing#asBool.

--- core/src/main/scala/algebra/ring/BoolRing.scala
+++ core/src/main/scala/algebra/ring/BoolRing.scala
@@ -7,10 +7,25 @@ import lattice.Bool
  * A Boolean ring is a ring whose multiplication is idempotent, that is
  * `a⋅a = a` for all elements ''a''. This property also implies `a+a = 0`
  * for all ''a'', and `a⋅b = b⋅a` (commutativity of multiplication).
+ *
+ * Every Boolean ring is equivalent to a Boolean algebra.
+ * See [[BoolRing.asBool]] for details.
  */
 trait BoolRing[A] extends Any with CommutativeRing[A] { self =>
   override final def negate(x: A): A = x
 
+  /**
+   * Every Boolean ring gives rise to a Boolean algebra:
+   *  - 0 and 1 are preserved;
+   *  - ring multiplication (`times`) corresponds to `and`;
+   *  - ring addition (`plus`) corresponds to `xor`;
+   *  - `a or b` is then defined as `a xor b xor (a and b)`;
+   *  - complement (`¬a`) is defined as `a xor 1`.
+   *
+   * `BoolRing.asBool.asCommutativeRing` gives back the original `BoolRing`.
+   *
+   * @see [[algebra.lattice.Bool.asCommutativeRing]]
+   */
   def asBool: Bool[A] = new Bool[A] {
     def zero: A = self.zero
     def one: A = self.one
