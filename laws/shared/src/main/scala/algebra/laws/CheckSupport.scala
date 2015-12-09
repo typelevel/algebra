3b471f0f4365beb223fecd6793991f1c3e4a80d2
Rüdiger Klaehn <rklaehn@gmail.com>
2015-12-09 23:21:55 +0100

    Moved Rat into algebra.laws test scope
    Also moved Rat arbitrary directly into Rat companion object. CheckSupport
    is now empty. Remove?

--- laws/shared/src/main/scala/algebra/laws/CheckSupport.scala
+++ laws/shared/src/main/scala/algebra/laws/CheckSupport.scala
@@ -1,10 +1,5 @@
 package algebra.laws
 
-import algebra.std.Rat
-
-import org.scalacheck._
-import Arbitrary.arbitrary
-
 /**
  * This object contains Arbitrary instances for types defined in
  * algebra.std, as well as anything else we'd like to import to assist
@@ -14,8 +9,4 @@ import Arbitrary.arbitrary
  * Arbitrary instances in companions.)
  */
 object CheckSupport {
-  implicit val ratArbitrary =
-    Arbitrary(for {
-      (n, d) <- arbitrary[(BigInt, BigInt)] if d != 0
-    } yield Rat(n, d))
 }
