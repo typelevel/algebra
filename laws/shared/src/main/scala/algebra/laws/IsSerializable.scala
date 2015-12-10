9723542aa6e79f114ee44152060c2839bfc7ba83
Sam Ritchie <sritchie09@gmail.com>
2015-12-10 09:06:10 -0700

    first pass.

--- laws/shared/src/main/scala/algebra/laws/IsSerializable.scala
+++ laws/shared/src/main/scala/algebra/laws/IsSerializable.scala
@@ -1,8 +1,8 @@
 package algebra.laws
 
-sealed trait IsSerializable[T]
-case class Is[T]() extends IsSerializable[T]
-case class IsNot[T]() extends IsSerializable[T]
+sealed trait IsSerializable[+T]
+case class Is[+T]() extends IsSerializable[T]
+case class IsNot[+T]() extends IsSerializable[T]
 
 /**
   * ADT that allows users to skip the serialization tests for certain
@@ -13,5 +13,5 @@ case class IsNot[T]() extends IsSerializable[T]
   * In your test's scope or your class's companion object.
   */
 object IsSerializable {
-  implicit def default[T]: IsSerializable[T] = Is()
+  // implicit def default[T]: IsSerializable[T] = Is()
 }
