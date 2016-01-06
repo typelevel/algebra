9d30bd4ece4e0a7096f42a19280ec018b3e7a710
Sam Ritchie <sritchie09@gmail.com>
2016-01-06 13:33:22 -0500

    make IsSerializable object private.

--- laws/shared/src/main/scala/algebra/laws/IsSerializable.scala
+++ laws/shared/src/main/scala/algebra/laws/IsSerializable.scala
@@ -6,7 +6,7 @@ import scala.util.DynamicVariable
   * Object with a dynamic variable that allows users to skip the
   * serialization tests for certain instances.
   */
-object IsSerializable {
+private[laws] object IsSerializable {
   val runTests = new DynamicVariable[Boolean](true)
   def apply(): Boolean = runTests.value
 }
