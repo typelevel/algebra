127572fc249f24e21350bc6d93ae23d28f17c845
Erik Osheim <erik@stripe.com>
2016-09-09 12:07:33 -0400

    Add instances for BigDecimal and BitSet.
    This commit also adds a static method for computing GCDs of BigDecimal
    values (ported from Spire), and standardizes the methods used in the
    set instances.

--- core/src/main/scala/algebra/instances/set.scala
+++ core/src/main/scala/algebra/instances/set.scala
@@ -17,10 +17,10 @@ trait SetInstances extends cats.kernel.instances.SetInstances {
 }
 
 class SetLattice[A] extends GenBool[Set[A]] {
-  def zero: Set[A] = Set.empty[A]
-  def or(lhs: Set[A], rhs: Set[A]): Set[A] = lhs.union(rhs)
-  def and(lhs: Set[A], rhs: Set[A]): Set[A] = lhs.intersect(rhs)
-  def without(lhs: Set[A], rhs: Set[A]): Set[A] = lhs.diff(rhs)
+  def zero: Set[A] = Set.empty
+  def or(lhs: Set[A], rhs: Set[A]): Set[A] = lhs | rhs
+  def and(lhs: Set[A], rhs: Set[A]): Set[A] = lhs & rhs
+  def without(lhs: Set[A], rhs: Set[A]): Set[A] = lhs -- rhs
 }
 
 class SetSemiring[A] extends Semiring[Set[A]] {
@@ -31,6 +31,6 @@ class SetSemiring[A] extends Semiring[Set[A]] {
 
 class SetBoolRng[A] extends BoolRng[Set[A]] {
   def zero: Set[A] = Set.empty
-  def plus(x: Set[A], y: Set[A]): Set[A] = (x diff y) | (y diff x)
+  def plus(x: Set[A], y: Set[A]): Set[A] = (x -- y) | (y -- x)
   def times(x: Set[A], y: Set[A]): Set[A] = x & y
 }
