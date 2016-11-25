b01cb79e4f94c31172033c7e3b1796ad939ebe5b
Denis Rosset <physics@denisrosset.com>
2016-11-25 12:16:41 -0300

    Added missing commutative instances (semiring/rng).

--- core/src/main/scala/algebra/ring/CommutativeRing.scala
+++ core/src/main/scala/algebra/ring/CommutativeRing.scala
@@ -6,7 +6,7 @@ import scala.{specialized => sp}
 /**
  * CommutativeRing is a Ring that is commutative under multiplication.
  */
-trait CommutativeRing[@sp(Int, Long, Float, Double) A] extends Any with Ring[A] with CommutativeRig[A]
+trait CommutativeRing[@sp(Int, Long, Float, Double) A] extends Any with Ring[A] with CommutativeRig[A] with CommutativeRng[A]
 
 object CommutativeRing extends RingFunctions[CommutativeRing] {
   @inline final def apply[A](implicit r: CommutativeRing[A]): CommutativeRing[A] = r
