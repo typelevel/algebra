4dbddeb2b32da5998805aa4ad5fac908e449f11d
Lars Hupel <lars.hupel@mytum.de>
2017-05-14 18:12:09 +0200

    deploy microsite to gh-pages

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+git config --global user.email "bot@typelevel.org"
+git config --global user.name "Typelevel Bot"
+git config --global push.default simple
+
+./sbt docs/publishMicrosite
